1ffc4c1a07be95ca615d4cd592edcaecc66ecf3d
street-magic <nerdal121@gmail.com>
2020-07-20 14:11:32 +0300

    added mesh visualization

--- mesh_generator/C/main.c
+++ mesh_generator/C/main.c
@@ -2,7 +2,8 @@
 #include <limits.h>
 #include <float.h>
 #include <string.h>
-#include "Node.h" //Nodes amd Edges
+#include <stdbool.h>
+#include "Node.h" //Nodes and Edges
 #define INF DBL_MAX
 
 int main(int argc, char *argv[]) {
@@ -18,24 +19,42 @@ int main(int argc, char *argv[]) {
 	int BUFSIZE = 99;
 	char buff[BUFSIZE];
 
+	bool python = true;
+
+	Node* first_cont_node = NULL;
 	while(fgets(buff, BUFSIZE - 1, fp) != NULL) {
 		if (buff[0] != '\n') {
 			Node* last_node = &node_data;
 			while (last_node->next != NULL) 
 				last_node = last_node->next;
+
 			double x, y;
 			sscanf(buff, "%lf %lf", &x, &y);
 			Node* new_node = add_node(&node_data, x, y);
-			if (last_node != &node_data) {
+			if (first_cont_node == NULL){
+				first_cont_node = new_node;
+			} else {
 				add_edge(&edge_data, last_node, new_node);
 			}
 		} else {
-			Node* last_node = &node_data;
+			//printf("closing edge\n");
+			Node* last_node = first_cont_node;
 			while (last_node->next != NULL)
 				last_node = last_node->next;
-			add_edge(&edge_data, last_node, edge_data.next->begins_at);
+			//printf("%lf %lf\n", last_node->x, last_node->y);
+			add_edge(&edge_data, last_node, first_cont_node);
+			first_cont_node = NULL;
 		}
 	}
 	fclose(fp);
+
+	Edge* cur_edge = edge_data.next;
+	while (cur_edge != NULL) {
+		if (python) {
+			printf("plt.plot([%lf,%lf],",cur_edge->begins_at->x, cur_edge->ends_at->x);
+			printf("[%lf,%lf],'k-')\n",cur_edge->begins_at->y, cur_edge->ends_at->y);
+		}
+		cur_edge = cur_edge->next;
+	}
 	return 0;
 }
